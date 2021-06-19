72504dbc1077fc04b39ecc00f8dded99ea01bd36
fullfilm2021 <86101256+fullfilm2021@users.noreply.github.com>
2021-06-19 19:49:08 +0530

    init 14

--- web/src/App.vue
+++ web/src/App.vue
@@ -50,62 +50,73 @@
 			</v-col>
 		</v-row> -->
 		<v-content>
-		<v-card class="mx-auto mt-5" max-width="344" tile elevation="2">
-			<v-list-item three-line>
-				<v-list-item-content>
-					<div class="text-overline mb-4">ADMIN</div>
-					<v-list-item-title class="text-h5 mb-1">
-						Avinandan Jana
-					</v-list-item-title>
-					<v-list-item-subtitle>
-						admin@avinandan.com
-					</v-list-item-subtitle>
-				</v-list-item-content>
+			<v-card
+				class="mx-auto mt-2"
+				max-width="400"
+				tile
+				elevation="5"
+				dark
+				color="teal darken-4"
+			>
+				<v-list-item three-line>
+					<v-list-item-content>
+						<div class="text-overline mb-4">ADMIN</div>
+						<v-list-item-title class="text-h5 mb-1">
+							Avinandan Jana
+						</v-list-item-title>
+						<v-list-item-subtitle>
+							admin@avinandan.com
+						</v-list-item-subtitle>
+					</v-list-item-content>
 
-				<v-list-item-avatar tile size="80" color="grey">
-					<img src="https://i.imgur.com/RXzPMXJ.png" alt="Avinandan" />
-				</v-list-item-avatar>
-			</v-list-item>
+					<v-list-item-avatar tile size="100">
+						<img
+							src="https://i.imgur.com/M6NHJri.png"
+							alt="Avinandan Jana"
+						/>
+					</v-list-item-avatar>
+				</v-list-item>
 
-			<v-card-actions>
-				<v-btn
-					icon
-					tag="a"
-					href="https://www.facebook.com/avinandanjana"
-					target="_blank"
-					><v-icon color="#4267B2">mdi-facebook</v-icon></v-btn
-				>
-				<v-btn
-					icon
-					tag="a"
-					href="https://wa.me/918016181551"
-					target="_blank"
-					><v-icon color="#4ac959">mdi-whatsapp</v-icon></v-btn
-				>
-				<v-btn
-					icon
-					tag="a"
-					href="https://www.instagram.com/avinandanjana"
-					target="_blank"
-					><v-img
-						src="https://brandpalettes.com/wp-content/uploads/2018/10/Instagram-300x300.png"
-						max-width="24"
-					></v-img
-				></v-btn>
-				<v-btn
-					icon
-					tag="a"
-					href="https://www.twitter.com/ghanajana"
-					target="_blank"
-					><v-icon color="#1da1f2">mdi-twitter</v-icon></v-btn
-				>
+				<v-card-actions>
+					<v-btn
+						icon
+						tag="a"
+						href="https://www.facebook.com/avinandanjana"
+						target="_blank"
+						><v-icon color="#4267B2">mdi-facebook</v-icon></v-btn
+					>
+					<v-btn
+						icon
+						tag="a"
+						href="https://wa.me/918016181551"
+						target="_blank"
+						><v-icon color="#4ac959">mdi-whatsapp</v-icon></v-btn
+					>
+					<v-btn
+						icon
+						tag="a"
+						href="https://www.instagram.com/avinandanjana"
+						target="_blank"
+						><v-img
+							src="https://brandpalettes.com/wp-content/uploads/2018/10/Instagram-300x300.png"
+							max-width="24"
+						></v-img
+					></v-btn>
+					<v-btn
+						icon
+						tag="a"
+						href="https://www.twitter.com/ghanajana"
+						target="_blank"
+						><v-icon color="#1da1f2">mdi-twitter</v-icon></v-btn
+					>
 
-				<v-btn icon @click="snack = true">
-					<v-icon color="#FF0000">mdi-youtube</v-icon>
-				</v-btn>
-			</v-card-actions>
-		</v-card>
-		 <router-view /> </v-content>
+					<v-btn icon @click="snack = true">
+						<v-icon color="#FF0000">mdi-youtube</v-icon>
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+			<router-view />
+		</v-content>
 		<LoginDialog :show="showAuthInput" />
 		<v-snackbar v-model="snack" :timeout="time" color="error"
 			>Sorry, currently its not available!</v-snackbar
